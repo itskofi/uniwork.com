ede0cbb5da4b571b4c9fc276baa3ec3170dcffba
Kofi <kofi_offei@icloud.com>
2023-09-02 10:14:14 +0200

    feat: improved jobboard layout

--- resources/views/livewire/jobboard.blade.php
+++ resources/views/livewire/jobboard.blade.php
@@ -3,13 +3,17 @@
         <div class="">{{ $jobs->count() }} Jobs found</div>
         <div class="flex flex-col space-y-2">
             @foreach ($jobs as $job)
-                <div class="border border-black rounded-xl">
-                    <div class="">{{ $job['title'] }}</div>
-                    <div class="">{{ $job['description'] }}</div>
-                    <div class="">{{ $job['place'] }}</div>
-                    <div class="">{{ $job['working_hours'] }}</div>
-                    <div class="">{{ $job['salary'] }}</div>
-                    <div class="">{{ $job['contact_person'] }}</div>
+                <div class="border border-black rounded-xl px-2">
+                    <a href="/viewjob=1" class="">
+                        <div class="">
+                            <span class="font-semibold">{{ $job['title'] }},</span>
+                            <span class="font-semibold">{{ $job['place'] }}</span>
+                        </div>
+                        <div class="">Working Hours: {{ $job['working_hours'] }}</div>
+                        <div class="">Salary: {{ $job['salary'] }}</div>
+                        <div class="">Contact Person: {{ $job['contact_person'] }}</div>
+                        <div class="">Company: {{ $job['company'] }}</div>
+                    </a>
                 </div>
             @endforeach
         </div>
