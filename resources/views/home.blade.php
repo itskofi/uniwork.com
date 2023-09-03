31e78a18782de19586b66eb3152c977447b44ea1
Kofi <kofi_offei@icloud.com>
2023-09-03 04:14:13 +0200

    docs: german to english

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="flex flex-col space-y-1 p-4">
-        <x-home.search-dropdown label="Was" placeholder="Jobtitel, Stichworte oder Unternehmen" />
-        <x-home.search-dropdown label="Wo" placeholder="Bundesland, Stadt oder Postleitzahl" />
+        <x-home.search-dropdown label="What" placeholder="Job title, keywords, or company" />
+        <x-home.search-dropdown label="Where" placeholder="Federal state, city, or postal code" />
     </div>
     <div class="p-4">
         <livewire:jobboard />
