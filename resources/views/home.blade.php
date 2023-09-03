765bf3993c3bfffa2d47f3eeb37f93c5b627a967
Kofi <kofi_offei@icloud.com>
2023-09-03 03:41:54 +0200

    feat: extracted a new layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.basic')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <!-- Styles -->
-    @livewireStyles
-</head>
-
-<body class="font-sans antialiased">
-
-    <div class="w-full">
-        <div class="max-w-5xl mx-auto">
-            <div class="flex flex-col space-y-1 p-4">
-                <x-home.search-dropdown label="Was" placeholder="Jobtitel, Stichworte oder Unternehmen" />
-                <x-home.search-dropdown label="Wo" placeholder="Bundesland, Stadt oder Postleitzahl" />
-            </div>
-            <div class="p-4">
-                <livewire:jobboard/>
-            </div>
-        </div>
+@section('content')
+    <div class="flex flex-col space-y-1 p-4">
+        <x-home.search-dropdown label="Was" placeholder="Jobtitel, Stichworte oder Unternehmen" />
+        <x-home.search-dropdown label="Wo" placeholder="Bundesland, Stadt oder Postleitzahl" />
     </div>
-
-    @livewireScripts
-</body>
-
-</html>
+    <div class="p-4">
+        <livewire:jobboard />
+    </div>
+@endsection
